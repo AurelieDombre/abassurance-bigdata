88a6ef0b4e06b446430e8610b67f01f692763b07
AurelieDombre <au.dombre@gmail.com>
2026-09-23 17:12:10 +0200

    essaie avec un script minimal

--- app/app.py
+++ app/app.py
@@ -1,5 +1,27 @@
 import streamlit as st
+import pandas as pd
 
-st.title("AbAssurance - Big Data")
+st.set_page_config(
+    page_title="Big Data Dashboard",
+    layout="wide"
+)
 
-st.write("Application de visualisation et de prédiction")
+st.title("Big Data Dashboard")
+
+@st.cache_data
+def load_data():
+    return pd.read_csv(
+    "/data/output/dataClean_fusion/clients",
+    sep=";"
+)
+
+data = load_data()
+
+st.subheader("Clients")
+
+st.write(f"Nombre de clients : **{len(data)}**")
+
+st.dataframe(
+    data,
+    use_container_width=True
+)
